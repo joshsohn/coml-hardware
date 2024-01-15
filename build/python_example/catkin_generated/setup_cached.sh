cc039fd2abcaa32d086cc81c4453e8298e0ec26a
Josh Sohn <jcsohn@mit.edu>
2024-01-15 00:44:31 -0500

    can get state of quadrotor

--- build/python_example/catkin_generated/setup_cached.sh
+++ build/python_example/catkin_generated/setup_cached.sh
@@ -9,6 +9,7 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/raphaelpeabody/r/simulation_ws/devel/.private/python_example:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/raphaelpeabody/r/simulation_ws/devel/.private/python_example/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/raphaelpeabody/r/simulation_ws/devel/.private/python_example/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/raphaelpeabody/r/simulation_ws/build/python_example'
 export PYTHONPATH="/home/raphaelpeabody/r/simulation_ws/devel/.private/python_example/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/raphaelpeabody/r/simulation_ws/devel/.private/python_example/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
--- src/python_example/src/python_example/src/python_example/foo.py
+++ src/python_example/src/python_example/src/python_example/foo.py
@@ -2,17 +2,17 @@ print("  importing bar_fn, baz_fn from simon_pythonsubpackage1")
 from .simon_pythonsubpackage1 import bar_fn, baz_fn
 print("  imported bar_fn, baz_fn from simon_pythonsubpackage1")
 import rospy
-from std_msgs.msg import String
+from snapstack_msgs.msg import State
 
 def callback(data):
-    rospy.loginfo("Received: %s", data.data)
+    rospy.loginfo("Received: %s", data)
 
 def main():
     # Initialize the ROS node with a default name
     rospy.init_node('my_node_name')
 
     # Node logic
-    rospy.Subscriber('state', String, callback)
+    rospy.Subscriber('state', State, callback)
 
     # Spin to keep the node alive and process callbacks
     rospy.spin()
